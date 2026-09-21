5ff9b2637e52298e9cfb148a4ba6df46713ce16f
Aceva Arie Sadewa <acevaaaariesadewa@gmail.com>
2026-09-21 12:51:49 +0700

    Suppress PHP 8.2 deprecated warnings

--- api/index.php
+++ api/index.php
@@ -1,8 +1,7 @@
 <?php
 // Bridge script untuk Vercel Serverless PHP Execution
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT & ~E_NOTICE);
+ini_set('display_errors', 0);
 
 chdir(__DIR__ . '/..');
 

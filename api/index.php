ed3bd6c11dfdce722b5f2d99b80ca41d78ffc45e
Aceva Arie Sadewa <acevaaaariesadewa@gmail.com>
2026-09-21 12:45:41 +0700

    Enable error reporting for Vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,9 @@
 <?php
 // Bridge script untuk Vercel Serverless PHP Execution
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../index.php';
 
